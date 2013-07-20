38dba81ae8accd308bc1e276b269a7a8cd889dbe
ryanbartley <ryanbartley@gmail.com>
2013-07-20 18:01:29 -0400

    "Initial commit"

---
+++ src/CinderTCPServerApp.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class CinderTCPServerApp : public AppNative {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void CinderTCPServerApp::setup()
+{
+}
+
+void CinderTCPServerApp::mouseDown( MouseEvent event )
+{
+}
+
+void CinderTCPServerApp::update()
+{
+}
+
+void CinderTCPServerApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP_NATIVE( CinderTCPServerApp, RendererGl )
